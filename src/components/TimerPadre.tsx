165ebfe46281e3df2cdac57decf30a99d97e86da
JesusCamaRay <jesuscamaraymundo@gmail.com>
2022-05-08 17:39:46 -0500

    feature: useEffect + useRef

---
+++ src/components/TimerPadre.tsx
@@ -0,0 +1,23 @@
+import React,{useState} from 'react'
+import { Timer } from './Timer'
+
+export const TimerPadre = () => {
+  const [miliseconds, setMiliseconds] = useState(1000)
+  return (
+    <>
+      <span>Milisegundos {miliseconds}</span>
+      <br/>
+      <button className='btn btn-outline-success'
+        onClick={()=>setMiliseconds(1000)}
+      >
+        1000
+      </button>
+      <button className='btn btn-outline-success'
+      onClick={()=>setMiliseconds(2000)}
+      >
+        2000
+      </button>
+      <Timer miliseconds={miliseconds}/>
+    </>
+  )
+}
